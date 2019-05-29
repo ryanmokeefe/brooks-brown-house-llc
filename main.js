df6b3f4272423ea09e997240ed76da91034d1f24
Ryan O'Keefe <ryan@ryanokeefe.net>
2019-05-28 22:39:53 -0400

    mobile start

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(window).resize(function(){
     offsetNav = $("nav").offset().top;
 })
 
-$(window).scroll(function() {
+function stickyNav() {
     
     var scrolltop = $(window).scrollTop();
     var dist = (offsetNav - scrolltop);
@@ -21,4 +21,32 @@ $(window).scroll(function() {
         $("nav").removeClass("fixedNav");
         $("nav").addClass("scrollNav");
     }
+};
+
+
+$(window).scroll(function() {
+    stickyNav();
+    stickyLogo();
 });
+
+// moving BBH logo
+
+var offsetLogo = $(".headerLogo").offset().top;
+$(window).resize(function(){
+    offsetLogo = $(".headerLogo").offset().top;
+})
+
+function stickyLogo() {
+    var scrolltop = $(window).scrollTop();
+    var dist = (offsetLogo - scrolltop);
+
+    if (dist <= 0)
+    {
+        $(".headerLogo").removeClass("scrollLogo");
+        $(".headerLogo").addClass("fixedLogo");
+    } else 
+    {
+        $(".headerLogo").removeClass("fixedLogo");
+        $(".headerLogo").addClass("scrollLogo");
+    }
+}

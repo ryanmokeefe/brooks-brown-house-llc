ee211b678e66b3e93c2484c26675f21d3ab67643
Ryan O'Keefe <ryan@ryanokeefe.net>
2019-06-13 12:07:57 -0400

    mobile

--- main.js
+++ main.js
@@ -1,80 +1,52 @@
-
-// Fixed NavBar
-
-var offsetNav = $("nav").offset().top;
-$(window).resize(function(){
-    offsetNav = $("nav").offset().top;
-})
-
-function stickyNav() {
-    
-    var scrolltop = $(window).scrollTop();
-    var dist = (offsetNav - scrolltop);
-
-    if (dist <= 0)
-    {
-        $("nav").removeClass("scrollNav");
-        $("nav").addClass("fixedNav");
-    } else 
-    {
-        $("nav").removeClass("fixedNav");
-        $("nav").addClass("scrollNav");
-    }
-};
-
-
-// $(window).scroll(function() {
-//     // stickyNav();
-//     stickyLogo();
-// });
-
-// // moving BBH logo
-
-// var offsetLogo = $(".headerLogo").offset().top;
-// $(window).resize(function(){
-//     offsetLogo = $(".headerLogo").offset().top;
-// })
-
-// function stickyLogo() {
-//     var scrolltop = $(window).scrollTop();
-//     var dist = (offsetLogo - scrolltop);
-
-//     if (dist <= 0)
-//     {
-//         $(".headerLogo").removeClass("scrollLogo");
-//         $(".headerLogo").addClass("fixedLogo");
-//     } else 
-//     {
-//         $(".headerLogo").removeClass("fixedLogo");
-//         $(".headerLogo").addClass("scrollLogo");
-//     }
-// }
-
-// 
-
-
-const makeSquares = function() {
-    const squares = $('.squares');
-    for (i = 0; i < 10; i++) {
-        let row = '<div class="square-row square-row' + i + '">';
-        for (i = 0; i < 10; i++) {
-            $(row).append('<div class="square-new square-new' + i + '"></div>')
-            console.log("square-new" + i)
-        }
-        $(row).append('</div>');
-        $(squares).append(row);
-        console.log(row)
-
-    }
-} 
-
 // Mobile Nav:
-
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+// Carousel 
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+      
+// Fixed NavBar
+// var offsetNav = $("#missionStatement").offset().top;
+// $(window).resize(function(){
+//     offsetNav = $("#missionStatement").offset().top;
+// })
+// console.log(offsetNav)
+// function stickyNav() {
+//     var scrolltop = $(window).scrollTop();
+//     console.log(scrolltop)
+//     var dist = (offsetNav - scrolltop);
+//     if (dist >= 0)
+//     {
+//         $(".nav-mobile").removeClass(".nav-m-scroll");
+//         $(".nav-open").removeClass(".nav-m-button-scroll");
+//     } else 
+//     {
+//         $(".nav-mobile").addClass(".nav-m-scroll");
+//         $(".nav-open").addClass(".nav-m-button-scroll");
+//     }
+// };
+// $(window).scroll(function() {
+//     stickyNav();
+// });
 
+// TODO: 
 
+// const makeSquares = function() {
+//     const squares = $('.squares');
+//     for (i = 0; i < 10; i++) {
+//         let row = '<div class="square-row square-row' + i + '">';
+//         for (i = 0; i < 10; i++) {
+//             $(row).append('<div class="square-new square-new' + i + '"></div>')
+//             console.log("square-new" + i)
+//         }
+//         $(row).append('</div>');
+//         $(squares).append(row);
+//         console.log(row)
+
+//     }
+// } 
 // $(document).ready(function() {
 //     makeSquares();
 // })
